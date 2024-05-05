3544652ffbe26a0af15b7fb57623d402dc7989a0
psarngadharan <100769787+psarngadharan@users.noreply.github.com>
2024-05-05 20:17:16 +0200

    Create gmx.sh

---
+++ scripts/gmx.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo 3 6 | gmx pdb2gmx -f name.pdb -o name_processed.gro
+gmx editconf -f name_processed.gro -o name_newbox.gro -c -d 1.0 -bt cubic
+gmx solvate -cp name_newbox.gro -cs spc216.gro -o name_solv.gro -p topol.top
+gmx grompp -f ions.mdp -c name_solv.gro -p topol.top -o ions.tpr
+gmx genion -s ions.tpr -o name_solv_ions.gro -p topol.top -pname NA -nname CL -neutral
+gmx grompp -f minim.mdp -c name_solv_ions.gro -p topol.top -o em.tpr
+gmx mdrun -v -deffnm em
+gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
+gmx mdrun -deffnm nvt
+gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
+gmx mdrun -deffnm npt
+gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md.tpr
+gmx mdrun -deffnm md
