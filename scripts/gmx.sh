042a65bd2508da281db02e31dc3da6bb5b31f218
psarngadharan <100769787+psarngadharan@users.noreply.github.com>
2024-05-06 08:11:04 +0200

    Update gmx.sh

--- scripts/gmx.sh
+++ scripts/gmx.sh
@@ -4,7 +4,7 @@ gmx editconf -f name_processed.gro -o name_newbox.gro -c -d 1.0 -bt cubic
 gmx solvate -cp name_newbox.gro -cs spc216.gro -o name_solv.gro -p topol.top
 gmx grompp -f ions.mdp -c name_solv.gro -p topol.top -o ions.tpr
 gmx genion -s ions.tpr -o name_solv_ions.gro -p topol.top -pname NA -nname CL -neutral
-gmx grompp -f minim.mdp -c name_solv_ions.gro -p topol.top -o em.tpr
+gmx grompp -f em.mdp -c name_solv_ions.gro -p topol.top -o em.tpr
 gmx mdrun -v -deffnm em
 gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
 gmx mdrun -deffnm nvt
