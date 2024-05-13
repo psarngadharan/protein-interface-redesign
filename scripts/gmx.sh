39378cf9f3e823f537b50703a263117514c4ba05
psarngadharan <100769787+psarngadharan@users.noreply.github.com>
2024-05-13 15:08:04 +0200

    Update gmx.sh

--- scripts/gmx.sh
+++ scripts/gmx.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-echo 6 | gmx pdb2gmx -f 6M0J_fixed.pdb -o name_processed.gro -water tip3p -ignh
-gmx editconf -f name_processed.gro -o name_newbox.gro -c -d 1.0 -bt cubic
-gmx solvate -cp name_newbox.gro -cs spc216.gro -o name_solv.gro -p topol.top
-gmx grompp -f ions.mdp -c name_solv.gro -p topol.top -o ions.tpr
-echo 13 | gmx genion -s ions.tpr -o name_solv_ions.gro -p topol.top -pname NA -nname CL -neutral
+echo 3 | gmx pdb2gmx -f all_prot.pdb -o name_processed.gro -water tip3p -ignh
+#gmx editconf -f name_processed.gro -o name_newbox.gro -c -d 1.0 -bt cubic
+#gmx solvate -cp name_newbox.gro -cs spc216.gro -o name_solv.gro -p topol.top
+#gmx grompp -f ions.mdp -c name_solv.gro -p topol.top -o ions.tpr
+#echo 13 | gmx genion -s ions.tpr -o name_solv_ions.gro -p topol.top -pname NA -nname CL -neutral#
 gmx grompp -f em.mdp -c name_solv_ions.gro -p topol.top -o em.tpr
 gmx mdrun -v -deffnm em
 gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
